e297dcc9b220243e62ada3bdd59c24fed1234f77
Timothy Lalli <trlalli92@gmail.com>
2020-07-20 00:38:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,7 +2,7 @@ class School
   
   attr_accessor :school, :roster, :student 
   
-  def initialize(school, roster = )
+  def initialize(school, roster)
     @school = school
     roster = {:grade, :student_name} 
   end
